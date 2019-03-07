828db5e23376eee2457873edcc09c8a013c3d1a0
Samuel Barata <oliveira3@icloud.com>
2019-03-07 11:49:02 +0000

    add ex02

---
+++ labs/lab03/ex02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    piramide(n);
+    return 0;
+}
+
+void piramide(int n){
+    int linhas;
+    for (linhas = 1; linhas <=n; linhas++){
+        
+    }
+}
