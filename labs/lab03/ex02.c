3dd02fdca4a6f2e1d90bc0910d0b37c2c81467de
Raw Potato <rawpotat0@localhost.localdomain>
2019-03-07 18:27:41 +0000

    finished

--- labs/lab03/ex02.c
+++ labs/lab03/ex02.c
@@ -8,8 +8,19 @@ int main(){
 }
 
 void piramide(int n){
-    int linhas;
-    for (linhas = 1; linhas <=n; linhas++){
-        
+    int linhas, carac, next;
+    for(linhas = 1; linhas <=n; linhas++){
+        next = 0;
+        for(carac = 1; carac <= n*2-1; carac++){
+            if(linhas+n-1 < carac || (carac < n)&&(carac+linhas<=n)){
+                printf("  ");
+            }
+            else{
+                if(carac <= n)next++;
+                else next--;
+                printf("%d ",next);
+                }
+        }
+        printf("\n");
     }
 }
