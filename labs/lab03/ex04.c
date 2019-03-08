dd7b42c500dde1760f81c4f17daaefe6d6e5f6e0
Samuel Barata <oliveira3@icloud.com>
2019-03-08 12:47:04 +0000

    ex04 finished

--- labs/lab03/ex04.c
+++ labs/lab03/ex04.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
 int main(){
-
+    int in, total, str[500],i, inside;
+    inside=0;
+    for(total=0;in!=EOF;total++){
+        in = getchar();
+        str[total] = in;
+    }
+    for(i=0;i<total-1;i++){
+        if(str[i] == ' '){
+            inside = 0;
+            putchar(str[i]);
+        }
+        else if(inside){putchar(str[i]);}
+        else if(inside == 0 && (str[i]!='0' && str[i]!=' ')){
+            inside = 1;
+            putchar(str[i]);
+        }
+        else if(inside == 0 && (str[i]=='0' && str[i+1]==' ')){
+            putchar(str[i]);
+        }
+    }
+    printf("\n");
     return 0;
 }
