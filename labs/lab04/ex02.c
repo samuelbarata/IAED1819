35cee92e99a509bfe097b21af772f06f47175df6
Samuel Barata <oliveira3@icloud.com>
2019-03-13 08:39:19 +0000

    finished

--- labs/lab04/ex02.c
+++ labs/lab04/ex02.c
@@ -2,14 +2,17 @@
 #define VECMAX 100
 
 int main(){
-    int n, i, vec[VECMAX], linha;
+    int n, i, vec[VECMAX], linha,max=0;
     scanf("%d", &n);
     for(i = 0;i<n;i++){
         scanf("%d", &vec[i]);
     }
-    for(linha=1;linha<=n;linha++){
+    for(i=0;i<n;i++){
+        if(vec[i]>max)max=vec[i];
+    }
+    for(linha=1;linha<=max;linha++){
         for(i=0;i<n;i++){
-            if(vec[i]<=linha){printf("*");}
+            if(vec[i]>=linha){printf("*");}
             else{printf(" ");}
         }
         printf("\n");
