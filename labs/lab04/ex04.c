48c93661515db7bb85929bf30cb1e01a64eac034
Samuel Barata <oliveira3@icloud.com>
2019-03-14 18:47:43 +0000

    nao acabado

--- labs/lab04/ex04.c
+++ labs/lab04/ex04.c
@@ -2,6 +2,19 @@
 #define MAX 80
 
 int main(){
-    
+    int st[MAX], i,j, end=1;
+    scanf("%s", st);
+    for(i = 0; st[i] != EOF; i++){printf("%d",i);}
+    for(j = 0; j == i; j++){
+        if (st[i] != st[j]){
+            end = 0;
+        }
+        i--;
+    }
+    if(end){
+        printf("yes\n");
+        return 0;
+    }
+    printf("no\n");
     return 0;
 }
