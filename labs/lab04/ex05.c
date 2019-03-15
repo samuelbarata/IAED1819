5f220896107dda5f2332ea4cc202dbaa5a1d37e2
Samuel Barata <oliveira3@icloud.com>
2019-03-15 12:30:02 +0000

    finished

--- labs/lab04/ex05.c
+++ labs/lab04/ex05.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #define MAX 80
 
+int leLinha(char s[]);
+
 int main(){
-    
-    return 0;
+    char s[MAX];
+    return leLinha(s);
+}
+
+int leLinha(char s[]){
+    int i, m;
+    for(i=0; s[i-1] != EOF ; i++){s[i] = getchar();}
+    m=i-1;
+    for(i=0; i < m; i++){printf("%c", s[i]);}
+    printf("\n");
+    return m;
 }
