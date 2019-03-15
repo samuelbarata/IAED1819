049e288ef6ecdfea816b92c8b2f79e8ef1df2586
Samuel Barata <oliveira3@icloud.com>
2019-03-15 12:51:30 +0000

    not finished

--- labs/lab04/ex06.c
+++ labs/lab04/ex06.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #define MAX 80
 
+int maiusculas(char s[]);
+
 int main(){
-    
-    return 0;
+    char s[MAX];
+    return maiusculas(s);
+}
+
+int maiusculas(char s[]){
+    int i, m, dif = 'a'-'A';
+    for(i=0; s[i-1] != EOF ; i++){s[i] = getchar();}
+    m=i-1;
+    for(i=0; i < m; i++){
+        if (s[i] > 'z'){
+            printf("%c", s[i]+dif);
+        }
+        else{
+            printf("%c", s[i]);
+        }
+    }
+    printf("\n");
+    return m;
 }
