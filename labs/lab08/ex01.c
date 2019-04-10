671372efbad4cf65362e4ec61f1bdd83d87ae0bb
Samuel Barata <oliveira3@icloud.com>
2019-04-10 08:45:08 +0100

    added ex01

---
+++ labs/lab08/ex01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#define max 80
+
+int main(){
+    char *p, string[max];
+    int i,len;
+    fgets(string,max,stdin);
+    p = string;
+    len = strlen(string);
+    for(i = 0; i<len; i++,p++){
+        printf("%s", p);
+    }
+    return 0;
+}
