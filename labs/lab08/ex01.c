03feff3a6b002767034c7dc35d3c1387e3b1d245
samuelbarata <oliveira3@icloud.com>
2019-04-26 12:11:18 +0100

    enhancement

--- labs/lab08/ex01.c
+++ labs/lab08/ex01.c
@@ -4,11 +4,11 @@
 
 int main(){
     char *p, string[max];
-    int i,len;
     fgets(string,max,stdin);
     p = string;
-    len = strlen(string);
-    for(i = 0; i<len; i++,p++)
-        printf("%s\n", p);
+    while(*p!='\n'){
+        printf("%s", p);
+        p++;
+    }
     return 0;
 }
