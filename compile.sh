af46e053498ece9267be072e3e6392045d74c483
samuelbarata <oliveira3@icloud.com>
2019-04-29 22:00:31 +0100

    new compile script

--- compile.sh
+++ compile.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 clear
-if [[ $# != 1 ]] ; then
-	echo "Usage: $0 <file>"
-	return 1
+if [[ $# -lt 1 ]] ; then
+	echo "Usage: $0 <main-file> <src-file2> ..."
+	exit 1
 fi
 
 prog_name="${1}"
 cfile="${1}".c
+shift
 DIFF="diff --ignore-space-change --ignore-blank-lines"
 CC="gcc -ansi -Wall -Wextra -pedantic"
 MORE=" -fcolor-diagnostics -g -fdebug-info-for-profiling  -fsanitize=address"
 
 if [ ! -f "${cfile}" ]; then
 	echo "ERROR: file ${cfile} not found!"
-	return 1
+	exit 1
 fi
 
-${CC} -o ${prog_name} ${cfile} ${MORE}
+${CC} -o ${prog_name} ${cfile} $* ${MORE} 2> ${prog_name}.warnings
 rv_compile=$?
-if [ ${rv_compile} != 0 ];then
-	echo "ERROR: Compilation failed!"
-	return 1
+if [ ${rv_compile} != 0 ]; then
+	echo -e "${RED}ERROR: Compilation failed!${NC}"
+	cat ${prog_name}.warnings
+	rm -f ${prog_name}.warnings
+	exit 1
+
+elif [ -s ${prog_name}.warnings ]; then
+    echo -e "${YELLOW}Program ${prog_name} compiled with warnings and running:${NC}"
+    cat ${prog_name}.warnings
+
 else
-	echo "Program successfully compiled..."
-	return 0
+	echo -e "${GREEN}Program ${prog_name} successfully compiled and running:${NC}"
 fi
+
+./${prog_name}
+output=$?
+if [ ${output} != 0 ]; then
+	echo "ERROR: Program did not return 0!"
+else
+	echo "Program successfully ran"
+fi
+rm -f ${prog_name}.warnings
+exit 1
