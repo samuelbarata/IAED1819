a784c159fe2bd1d46e173a2d1bec307406eb0335
Raw Potato <rawpotat0@localhost.localdomain>
2019-04-30 19:33:53 +0100

    fixed compile script

--- compile.sh
+++ compile.sh
@@ -5,12 +5,18 @@ if [[ $# -lt 1 ]] ; then
 	exit 1
 fi
 
+RED='\033[0;31m'
+GREEN='\033[0;10m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 prog_name="${1}"
 cfile="${1}".c
 shift
 DIFF="diff --ignore-space-change --ignore-blank-lines"
 CC="gcc -ansi -Wall -Wextra -pedantic"
-MORE=" -fcolor-diagnostics -g -fdebug-info-for-profiling  -fsanitize=address"
+MORE=" -g -fsanitize=address"
 
 if [ ! -f "${cfile}" ]; then
 	echo -e "${RED}ERROR: file ${cfile} not found!${NC}"
@@ -26,18 +32,22 @@ if [ ${rv_compile} != 0 ]; then
 	exit 1
 
 elif [ -s ${prog_name}.warnings ]; then
-    echo -e "${YELLOW}Program ${prog_name} compiled with warnings${NC}"
+    echo -e "${YELLOW}Program ${NC}${GREEN}${prog_name}${NC}${YELLOW} compiled with warnings${NC}"
     cat ${prog_name}.warnings
 	echo "running:"
 
 else
-	echo -e "${GREEN}Program ${prog_name} successfully compiled and running:${NC}"
+	echo -e "${BLUE}Program ${prog_name} successfully compiled and running:${NC}"
 fi
 
 ./${prog_name}
 output=$?
-
-echo -e "${GREEN}Program successfully ran and return ${NC}${RED}${output}${NC}"
+if [ ${output} != 0 ]; then
+	echo -e "${YELLOW}Program ran and return ${NC}${RED}${output}${NC}"
+else
+	echo -e "${GREEN}Program successfully ran${NC}"
+fi
 
 rm -f ${prog_name}.warnings
+rm -f ${prog_name}
 exit 1
